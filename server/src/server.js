b5c7338ba9ceac235bc7f9a0d91384b510f00a21
Lou Vang <louvangdev@gmail.com>
2023-07-27 21:45:31 -0500

    Initial server setup

---
+++ server/src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  require('dotenv').config({
+    path: `${__dirname}/.env.prod`,
+  });
+} else {
+  require('dotenv').config({
+    path: `${__dirname}/.env.dev`,
+  });
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
